feb64359c383e73ff2448e8f97c5e79bbe22f7b8
Danila Evteev <EvteevDanilaAndreevich@yandex.ru>
2022-07-19 20:03:21 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+         int x = 8200; // стоимость одного билета
+
+         int y = x / 20; // вычисление кол-ва бонусов
+        System.out.println("Полученно миль = " + y);
+
+
+
+    }
+}
