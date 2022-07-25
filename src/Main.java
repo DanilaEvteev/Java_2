403a752a08d0daafc3e0b6e80262452701a30a5a
DanilaEvteev <103563125+DanilaEvteev@users.noreply.github.com>
2022-07-25 11:10:59 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
 
-         int x = 8200; // стоимость одного билета
+         int ticket = 8200; // стоимость одного билета
 
-         int y = x / 20; // вычисление кол-ва бонусов
-        System.out.println("Полученно миль = " + y);
+         int bonus = ticket / 20; // вычисление кол-ва бонусов
+        System.out.println("Полученно миль = " + bonus);
 
 
 
